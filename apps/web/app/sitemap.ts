9908ed3d29637a260725cab5363a63155cb4eac0
vishnuvardhan2114 <vishnuvardhan14102002@gmail.com>
2025-10-12 22:28:41 +0530

    Fix static export: add dynamic force-static to robots and sitemap

--- apps/web/app/sitemap.ts
+++ apps/web/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next'
 import { SERVICES } from '@/constants/popular-services.constants'
 
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://digidocs.com'
   
